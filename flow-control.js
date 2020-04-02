790ce6ddae876747e14c103aa801d1becf9f17a7
Drew Bartos <drewbartos23@gmail.com>
2020-04-02 19:20:13 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,2 +1,2 @@
-function basicTeenager(age) if (age === 13-19) { return "You are a teenager!" } else {
+function  basicTeenager(age) if (age === 13-19) { return "You are a teenager!" } else {
   age <= 12 , age >= 20 }
